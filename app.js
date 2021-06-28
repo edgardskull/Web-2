db76f474cd13a1ab68e91fbeba715d46fb8018bf
edgardskull <edgardskull@tutanota.com>
2021-06-28 13:44:51 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+//document.write("<h1>Web Amazon Nicho</h1>")
+
+//tipos de datos
+
+/*var frase = "Shut fuck off you piece of shit";
+var posicion = frase.indexOf("shit");
+
+var prubE = /putas/;
+
+var cadena = "Son todas putas"
+
+if (prubE.test(cadena)){
+    alert ("Si " + cadena);
+}
+console.log(posicion);
+*/
+
+
+
+if( a == 0){
+    if (b =! 1){
+
+    }
+}
+
+
